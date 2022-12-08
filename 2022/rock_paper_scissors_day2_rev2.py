1d4d381e154b2f8b0b6cfd7252291ea2555c4c82
ndriscoll20@github.com <ndriscoll20@gmail.com>
2022-12-07 22:33:30 -0500

    adding day 2 variant

--- 2022/rock_paper_scissors_day2_rev2.py
+++ 2022/rock_paper_scissors_day2_rev2.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed Dec  7 22:30:19 2022
+
+@author: Nick
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Wed Dec  7 21:43:04 2022
 
 @author: Nick
@@ -13,32 +20,21 @@ with open(filename) as fIn:
 # Scoring
 # Rock = X,A,1, Paper = Y,B,2, Scissors = Z,C,3
 # Loss = 0, Draw = 3, Win = 6
-
-def __init__(self, games):
-    self.games = games
-    
-def shapeScore(shape):
+total = []
+for game in games:
+    shape = game[2]
     if shape == 'X':
         score = 1
     elif shape == 'Y':
         score = 2
     elif shape == 'Z':
         score = 3
-    return score
 
-def outcomeScore(game):
     if game == 'A X' or 'B Y' or 'C Z':
         outcome = 3
     elif game == 'A Y' or 'B Z' or 'C A':
         outcome = 0
     elif game == 'A Z' or 'C Y' or 'B X':
         outcome = 6
-    return outcome
+    total.append(score+outcome)
 
-gameScores=[]
-for game in games:
-    out1 = shapeScore(game[2])
-    out2 = outcomeScore(game)
-    total = out1+out2
-    gameScores.append(total)
-    
